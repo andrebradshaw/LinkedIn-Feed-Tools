adc7069b3451e98460bc1353235f719601fe73e7
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-12-25 21:56:45 -0500

    Update autoLike_ifOg.js

--- autoLike_ifOg.js
+++ autoLike_ifOg.js
@@ -1,4 +1,5 @@
-//this will scroll through the first thousand items in your LinkedIn feed and click like (uncomment line 11) if the feed item is the from the original poster (ignores likes, follows, adverts)
+//this will scroll through the first thousand items in your LinkedIn feed and click like if the feed item is the from the original poster 
+//(ignores likes, follows, adverts)
 
 var doc = document;
 var socialBar = doc.getElementsByClassName('feed-shared-social-actions feed-shared-social-action-bar ember-view');
@@ -8,7 +9,7 @@ function feedReader(obj, n) {
         var likeBtn = obj.getElementsByTagName('button')[0];
 		var btnStatus = obj.getElementsByClassName('visually-hidden')[0].innerText;
 		if(btnStatus == 'Like' && obj.parentElement.firstElementChild.getAttribute('class') != 't-12 t-black--light t-normal ph4 pt3 pb2 feed-shared-header feed-shared-header--with-control-menu ember-view'){
-// 			likeBtn.click(btnStatus);
+			likeBtn.click();
 			console.log(obj.parentElement.firstElementChild.innerText);
 		}
       }, ((n + 1) * 2500));
